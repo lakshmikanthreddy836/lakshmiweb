9c8d4912eaa8f1bf1d70498da3cc6eaf32939843
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2024-02-24 21:46:08 +0530

    testing

--- src/pages/AddStation/components/FormAddStation.jsx
+++ src/pages/AddStation/components/FormAddStation.jsx
@@ -32,12 +32,12 @@ const Form_Add_Station = () => {
           </div>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+              <label className="block mb-2 text-sm font-medium text-gray-900 ">
                 State
               </label>
               <select
                 id="countries"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Choose a country</option>
                 <option value="US">United States</option>
