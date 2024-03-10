712978d818ba5548d67c5c73e882632b7ee2ac55
sahimakhtar <sahimakhtar1020@gmail.com>
2024-03-10 20:10:36 +0530

    changed ips

--- src/api-config/config.jsx
+++ src/api-config/config.jsx
@@ -4,5 +4,5 @@ export const serverUrl =
   NODE_ENV.toLowerCase() === "development"
     ? "https://railserver.hksoftware.in/api/v1/admin"
     : NODE_ENV.toLowerCase() === "prod"
-    ? "http://10.22.4.79/api/v1/admin"
+    ? "http://10.22.4.79:5000/api/v1/admin"
     : "http://localhost:3000/api/v1/admin";
