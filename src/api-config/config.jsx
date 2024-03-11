b49abb78bf63bab7c98d81e482a4a89c6f818dfa
sahimakhtar <sahimakhtar1020@gmail.com>
2024-03-12 00:07:34 +0530

    add new order

--- src/api-config/config.jsx
+++ src/api-config/config.jsx
@@ -4,5 +4,5 @@ export const serverUrl =
   NODE_ENV.toLowerCase() === "development"
     ? "https://railserver.hksoftware.in/api/v1/admin"
     : NODE_ENV.toLowerCase() === "prod"
-    ? "https://railkafeadmin.hksoftware.in/api/v1/admin"
+    ? "https://backend.asktomentor.com/api/v1/admin"
     : "http://localhost:3000/api/v1/admin";
